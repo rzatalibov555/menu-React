5a98e2a3e658afb06b046b4737a3446ce1ccae13
rzatalibov555 <sethubpro@gmail.com>
2024-09-24 18:03:15 +0400

    axios post, get, get by id, update put and patch, delete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,68 @@
-
 import './App.css';
-import Sidebar from './components/Sidebar/Sidebar';
+
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const BASE_URL = "http://localhost:3005"
 
 function App() {
+
+  const getAllUsers = async() => {
+    const response = await axios.get(`${BASE_URL}/users`)
+    console.log(response.data)
+  }
+
+  const getUserById = async(user_id) => {
+    const response = await axios.get(`${BASE_URL}/users/${user_id}`)
+    console.log(response.data)
+  }
+
+  const createuser = async(new_user) => {
+    const response = await axios.post(`${BASE_URL}/users`, new_user)
+    console.log("response", response.data)
+  }
+
+  const update_user = async(user_id, update_data)=> {
+   const response = await axios.patch(`${BASE_URL}/users/${user_id}`, update_data)
+  }
+
+
+  const delete_user_by_id = async(user_id) => {
+    const response = await axios.delete(`${BASE_URL}/users/${user_id}`)
+    console.log(response)
+  }
+
+  useEffect(()=>{
+    // getAllUsers()
+    
+    
+    // const new_user = {
+    //   "username": "Nureddin",
+    //   "password": "111"
+    // }
+    // createuser(new_user)
+
+
+
+    // put: melumati yeniden elave edirmiw kimi update edir
+    // patch: movcud melumatin lazim olan hisselerini update edir
+    // const update_data = {
+    //   "username": "Rza22",
+    //   "password": "123"
+    // }
+    // update_user(1,update_data)
+
+
+
+    // delete_user_by_id(2)
+    
+
+  },[])
+
   return (
-    <Sidebar/>
+    <div>
+
+    </div>
   );
 }
 
